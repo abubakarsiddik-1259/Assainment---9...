8b20af36966994fca30a7809ad21177de7df2774
Your Nameabubakarsiddik-1259 <abubakarsiddik1259@gmail.com>
2025-12-12 18:53:39 +0600

    Top Rated Providers add

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,8 +1,10 @@
 import React, { Suspense } from "react";
 import Bannar from "./Bannar";
 import Category from "./Category";
+import TopCategory from './TopCategory';
 
 const categoryPromise = fetch("/categories.json").then((res) => res.json());
+const topPromise = fetch("/top.json").then((res) => res.json());
 
 const HomePage = () => {
   return (
@@ -14,6 +16,13 @@ const HomePage = () => {
         fallback={<span className="loading loading-bars loading-xl"></span>}
       >
         <Category categoryPromise={categoryPromise}></Category>
+      </Suspense>
+
+      {/* ...... Top 5 Rating ...... */}
+      <Suspense
+        fallback={<span className="loading loading-bars loading-xl"></span>}
+      >
+        <TopCategory topPromise={topPromise}></TopCategory>
       </Suspense>
 
 
--- src/page/TopCategory.jsx
+++ src/page/TopCategory.jsx
@@ -1,28 +1,15 @@
+import React, { use } from "react";
+import { Link } from "react-router";
 
-
-import React, { use } from 'react';
-import { Link } from 'react-router';
-
-const Category = ({categoryPromise}) => {
-
-  const categoryData = use(categoryPromise)
-
-    return (
-
-      <div className="px-3 py-7 bg-gray-600 ">
-          <h1 className='px-6 text-3xl font-semibold text-gray-100'>Popular Skills</h1>
-      
-        <div className=' grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 px-3 py-4 '>
-
-        
-
-        {categoryData.map((category)=>(
-
-
-
-            
- <div className="p-2">
-            <div className="card  hover:shadow-lg transition bg-gradient-to-r from-[#44444E] to-[#44444E] h-full px- m-0 w-full shadow-sm ">
+const TopCategory = ({ topPromise }) => {
+  const top = use(topPromise);
+  return (
+    <div className="bg-[#50727B] pt-8 ">
+        <h1 className="px-6 py-4 text-3xl font-semibold text-gray-100">Top Rated skills</h1>
+      <div className=" gap-4 grid lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1">
+        {top.map((category) => (
+          <div className="p-2">
+            <div className="card  hover:shadow-lg transition bg-gradient-to-r from-[#49108B] to-[#49108B] h-full px- m-0 w-full shadow-sm ">
               <figure>
                 <img
                   className="p-2 rounded-xl h-50 w-full"
@@ -30,23 +17,23 @@ const Category = ({categoryPromise}) => {
                   alt="Shoes"
                 />
               </figure>
-              
+
               <div className="card-body text-start pl-5 px-4">
                 <h2 className=" text-start text-gray-300">
                   <span className="italic  text-gray-100 font-semibold">
-                    Skill Name 
-                  </span> 
-                   : {category.skillName}
+                    Skill Name
+                  </span>
+                  : {category.skillName}
                 </h2>
 
-                <p className='text-gray-300'>
+                <p className="text-gray-300">
                   <span className="italic  text-gray-100 font-semibold">
                     {" "}
                     Rating{" "}
                   </span>{" "}
                   : {category.rating}
                 </p>
-                <h2 className='text-gray-300'>
+                <h2 className="text-gray-300">
                   <span className="italic text-gray-100 font-semibold">
                     Price{" "}
                   </span>
@@ -54,7 +41,7 @@ const Category = ({categoryPromise}) => {
                 </h2>
                 <div className="card-actions justify-center">
                   <Link
-                    className="btn bg-[#787A91] border-0  w-full btn-primary"
+                    className="btn bg-[#27005D] border-0  w-full btn-primary"
                     to={`/category/${category.skillId}`}
                   >
                     View Detaile
@@ -63,17 +50,10 @@ const Category = ({categoryPromise}) => {
               </div>
             </div>
           </div>
-
-
-
-
-
-
         ))}
-
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Category;
+export default TopCategory;
